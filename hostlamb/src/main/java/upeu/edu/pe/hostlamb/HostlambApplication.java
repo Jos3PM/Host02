da83e83136f8ff2ea0363b407a4285af7e10c1ae
Jos3PM <just76111@hotmail.com>
2025-11-12 14:01:36 -0500

    Add files via upload

---
+++ hostlamb/src/main/java/upeu/edu/pe/hostlamb/HostlambApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.hostlamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostlambApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostlambApplication.class, args);
+	}
+
+}
---
+++ hostlamb/src/test/java/upeu/edu/pe/hostlamb/HostlambApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.hostlamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostlambApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
